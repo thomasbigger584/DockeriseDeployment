f74a5c00aa8925a35b3a71eb01e2ff10dd32a830
Thomas Bigger <thomas@performanceactive.com>
2018-07-10 10:52:50 +0100

    update to script to add to git afterwards

--- add_files.sh
+++ add_files.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-PROJECT_ROOT=/Users/thomasbigger/Desktop/projects/backend/skylark-backend
+PROJECT_ROOT=/Users/thomasbigger/Desktop/projects/backend/rimz
 
 mkdir -p $PROJECT_ROOT/src/main/docker/deployment/
 cp -a deployment/. $PROJECT_ROOT/src/main/docker/deployment/
@@ -8,5 +8,8 @@ cp -a deployment/. $PROJECT_ROOT/src/main/docker/deployment/
 cp bitbucket-pipelines.yml $PROJECT_ROOT
 
 cp Dockerfile $PROJECT_ROOT/src/main/docker/
+
+cd $PROJECT_ROOT
+git add .
 
 echo "Update the docker-compose service names and registry details for this app"
